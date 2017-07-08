3ecc2f0e77abf6843e400b51196d41b21dfa9012
Calen <lokera6@gmail.com>
2017-07-08 12:59:01 -0400

    save before pull

--- app/src/main/java/control/pot/coffee/fakecallgenerator/CallingActivity.java
+++ app/src/main/java/control/pot/coffee/fakecallgenerator/CallingActivity.java
@@ -205,11 +205,14 @@ public class CallingActivity extends AppCompatActivity {
                     CS.schedule();
                 }
 
+                finish();
+                /*
                 //Goes Back to Home
                 Intent startMain = new Intent(Intent.ACTION_MAIN);
                 startMain.addCategory(Intent.CATEGORY_HOME);
                 //startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(startMain);
+                */
 
             }
         });
