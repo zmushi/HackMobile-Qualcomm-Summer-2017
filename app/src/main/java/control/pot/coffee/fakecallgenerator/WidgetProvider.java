e10d98aa49cdbaa7a20016a3f9f3d924567ceb50
Calen <lokera6@gmail.com>
2017-07-08 11:04:11 -0400

    Work for widget

---
+++ app/src/main/java/control/pot/coffee/fakecallgenerator/WidgetProvider.java
@@ -0,0 +1,31 @@
+package control.pot.coffee.fakecallgenerator;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Created by loker on 7/8/2017.
+ */
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int count = appWidgetIds.length;
+        for (int i=0; i<count; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            //PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+            // Get the layout for the App Widget and attach an on-click listener to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            //views.setOnClickPendingIntent(R.id.button, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current App Widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
