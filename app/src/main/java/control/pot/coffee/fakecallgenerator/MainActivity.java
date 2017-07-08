f0e689a913e4bafef59806be2a72cd62938d5493
Mushi Zhou <393684209@qq.com>
2017-07-08 03:04:35 -0700

    Spinner UI Working

--- app/src/main/java/control/pot/coffee/fakecallgenerator/MainActivity.java
+++ app/src/main/java/control/pot/coffee/fakecallgenerator/MainActivity.java
@@ -1,28 +1,39 @@
 package control.pot.coffee.fakecallgenerator;
 
+
 import android.app.Fragment;
 import android.content.Intent;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity implements
-        ContactsListFragment.ContactsListFragmentInterface {
+        ContactsListFragment.ContactsListFragmentInterface,
+        AdapterView.OnItemSelectedListener {
 
     private EditText searchEditText;
     private String searchString;
     private Contact contact;
 
-
+    private static final String TAG  = "class";
     //Variable and constants to track fragment states
     private String fragState  = STATE_EMPTY;
     private static final String STATE_EMPTY     = "empty";      //No fragment displayed
     private static final String STATE_LIST      = "list";       //ContactsListFragment displayed
     private static final String STATE_DISPLAY   = "display";    //ContactDisplayFragment displayed
+
+    private int repeat;
+    private int delay;
+    private int interval;
 
 
     @Override
@@ -30,7 +41,48 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Spinner repeat_spinner = (Spinner) findViewById(R.id.repeat_spinner);
+        ArrayAdapter<CharSequence> r_adapter = ArrayAdapter.createFromResource(this,
+                R.array.repeat_array, android.R.layout.simple_spinner_item);
+        r_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        repeat_spinner.setAdapter(r_adapter);
+
+        Spinner delay_spinner = (Spinner) findViewById(R.id.delay_spinner);
+        ArrayAdapter<CharSequence> d_adapter = ArrayAdapter.createFromResource(this,
+                R.array.delay_array, android.R.layout.simple_spinner_item);
+        d_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        delay_spinner.setAdapter(d_adapter);
+
+        Spinner interval_spinner = (Spinner) findViewById(R.id.interval_spinner);
+        ArrayAdapter<CharSequence> i_adapter = ArrayAdapter.createFromResource(this,
+                R.array.interval_array, android.R.layout.simple_spinner_item);
+        i_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        interval_spinner.setAdapter(i_adapter);
+
         searchEditText = (EditText)findViewById(R.id.search_field);
+        repeat_spinner.setOnItemSelectedListener(this);
+        delay_spinner.setOnItemSelectedListener(this);
+        interval_spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+
+        switch(parent.getId()) {
+            case R.id.repeat_spinner: repeat = Integer.parseInt(parent.getItemAtPosition(pos).toString());
+                break;
+            case R.id.delay_spinner: delay = Integer.parseInt(parent.getItemAtPosition(pos).toString());
+                break;
+            case R.id.interval_spinner: interval = Integer.parseInt(parent.getItemAtPosition(pos).toString());
+        }
+        Log.v(TAG, Integer.toString(delay));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Nothing needs to be done
     }
 
     //called when contact is pressed in ContactsListFragment
